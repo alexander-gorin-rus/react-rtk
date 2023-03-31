f68a95c2084d8680f8b09cf10772df4236584f29
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2023-03-31 08:25:12 +0600

    стилизированы тексты загрузки и ошибки отображения чеков

--- src/modules/Cheques/ChequesList.tsx
+++ src/modules/Cheques/ChequesList.tsx
@@ -27,85 +27,89 @@ const ChequesList = () => {
     dispatch(getChequesListAction());
   }, [rerendering]);
   return (
-    <table>
-      <tr>
-        <th className="chequesHeader">Дата покупки</th>
-        <th className="chequesHeader">Киоск</th>
-        <th className="chequesHeader">Тип</th>
-        <th className="chequesHeader">Статус оплаты</th>
-        <th className="chequesHeader">Оплата</th>
-        <th className="chequesHeader">Сумма</th>
-        <th className="chequesHeader">Кол-во товара</th>
-        <th className="chequesHeader">Товары</th>
-        <th>Действия</th>
-      </tr>
-      {listStatus === ApiStatus.loading && <tbody>Идет загрузка чеков</tbody>}
-      {listStatus === ApiStatus.error && <tbody>Ошибка загрузки чеков</tbody>}
-      {listStatus === ApiStatus.ideal &&
-        list.map((cheque: ICheque) => (
-          <tr key={cheque.id}>
-            <td>
-              <ChequeDate cheque={cheque} />
-            </td>
-            <td>
-              <ChequeKiosk cheque={cheque} />
-            </td>
-            <td>
-              <ChequeType cheque={cheque} />
-            </td>
-            <td>
-              <ChequeStatus cheque={cheque} />
-            </td>
-            <td>
-              <ChequePays cheque={cheque} />
-            </td>
-            <td>
-              <ChequeSum cheque={cheque} />
-            </td>
-            <td>
-              <ChequeQuantity cheque={cheque} />
-            </td>
-            <td>
-              <ChequeGoods cheque={cheque} />
-            </td>
-            <td>
-              <div className={Styles.chequeItem}>
-                <input
-                  type="button"
-                  value="Создать"
-                  onClick={() => {
-                    setShowEditModal(true);
-                  }}
-                />
-                <input
-                  type="button"
-                  value="Редактировать"
-                  onClick={() => {
-                    setShowEditModal(true);
-                    setEditCheque(cheque);
-                  }}
-                />
-                <input
-                  type="button"
-                  value="Удалить"
-                  onClick={() => {
-                    dispatch(deleteChequeAction(cheque.id));
-                  }}
-                />
-              </div>
-              {showEditModal && (
-                <Modal
-                  onRerenderClick={() => setRerendering(!rerendering)}
-                  onClose={() => {
-                    setShowEditModal(false);
-                  }}
-                  cheque={editCheque}
-                />
-              )}
-            </td>
-          </tr>
-        ))}
-    </table>
+    <>
+      <div className={Styles["info-wrapper"]}>
+        {listStatus === ApiStatus.loading && <h2 className={Styles.loading}>Идет загрузка чеков</h2>}
+        {listStatus === ApiStatus.error && <h2 className={Styles.error}>Ошибка загрузки чеков</h2>}
+      </div>
+      <table>
+        <tr>
+          <th className="chequesHeader">Дата покупки</th>
+          <th className="chequesHeader">Киоск</th>
+          <th className="chequesHeader">Тип</th>
+          <th className="chequesHeader">Статус оплаты</th>
+          <th className="chequesHeader">Оплата</th>
+          <th className="chequesHeader">Сумма</th>
+          <th className="chequesHeader">Кол-во товара</th>
+          <th className="chequesHeader">Товары</th>
+          <th>Действия</th>
+        </tr>
+        {listStatus === ApiStatus.ideal &&
+          list.map((cheque: ICheque) => (
+            <tr key={cheque.id}>
+              <td>
+                <ChequeDate cheque={cheque} />
+              </td>
+              <td>
+                <ChequeKiosk cheque={cheque} />
+              </td>
+              <td>
+                <ChequeType cheque={cheque} />
+              </td>
+              <td>
+                <ChequeStatus cheque={cheque} />
+              </td>
+              <td>
+                <ChequePays cheque={cheque} />
+              </td>
+              <td>
+                <ChequeSum cheque={cheque} />
+              </td>
+              <td>
+                <ChequeQuantity cheque={cheque} />
+              </td>
+              <td>
+                <ChequeGoods cheque={cheque} />
+              </td>
+              <td>
+                <div className={Styles.chequeItem}>
+                  <input
+                    type="button"
+                    value="Создать"
+                    onClick={() => {
+                      setShowEditModal(true);
+                    }}
+                  />
+                  <input
+                    type="button"
+                    value="Редактировать"
+                    onClick={() => {
+                      setShowEditModal(true);
+                      setEditCheque(cheque);
+                    }}
+                  />
+                  <input
+                    type="button"
+                    value="Удалить"
+                    onClick={() => {
+                      dispatch(deleteChequeAction(cheque.id));
+                    }}
+                  />
+                </div>
+                {showEditModal && (
+                  <Modal
+                    onRerenderClick={() => setRerendering(!rerendering)}
+                    onClose={() => {
+                      setShowEditModal(false);
+                    }}
+                    cheque={editCheque}
+                  />
+                )}
+              </td>
+            </tr>
+          ))}
+      </table>
+    </>
   );
 };
 
